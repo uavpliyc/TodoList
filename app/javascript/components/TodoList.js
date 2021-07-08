81c760b992992d0a097c37ac2f2fd8a198f7a70f
uavpliyc <uavpliyc@gmail.com>
2021-07-09 08:57:07 +0900

    add update_is_completed

--- app/javascript/components/TodoList.js
+++ app/javascript/components/TodoList.js
@@ -97,6 +97,20 @@ function TodoList() {
     }
   }
 
+  const updateIsCompleted = (index, val) => {
+    var data = {
+      id: val.id,
+      name: val.name,
+      is_completed: !val.is_completed
+    }
+    axios.patch('/api/v1/todo/${val.id}', data)
+    .then(resp => {
+      const newTodos = [...todos]
+      newTodos[index].is_completed = resp.data.is_completed
+      setTodos(newTodos)
+    })
+  }
+
   return (
     <div>
       TodoList
