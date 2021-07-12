1f1cd86bbcac627551ec8436919185ded1f12add
uavpliyc <uavpliyc@gmail.com>
2021-07-13 08:19:41 +0900

    add import add_todo

--- app/javascript/components/AddTodo.js
+++ app/javascript/components/AddTodo.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Fisend } from 'react-icons/fi'
+
+const InputAndButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`
 
 function AddTodo() {
   return (
