8505d2da7a32c785ecf57f2ec6743504e9f99eb2
uavpliyc <uavpliyc@gmail.com>
2021-07-16 11:16:41 +0900

    add import edittodo

--- app/javascript/components/EditTodo.js
+++ app/javascript/components/EditTodo.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function EditTodo() {
   return (
