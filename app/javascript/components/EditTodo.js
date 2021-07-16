7aba408bc72ccd3e0c038e53001c2e139408c534
uavpliyc <uavpliyc@gmail.com>
2021-07-17 08:56:24 +0900

    add styled edittodo

--- app/javascript/components/EditTodo.js
+++ app/javascript/components/EditTodo.js
@@ -4,6 +4,53 @@ import styled from 'styled-components'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
+const InputName = styled.input`
+  font-size: 20px;
+  width: 100%;
+  height: 40px;
+  padding: 2px 7px;
+  margin: 12px 0;
+`
+
+const CurrentStatus = styled.div`
+  font-size: 19px;
+  margin: 8px 0 12px 0;
+  font-weight: bold;
+`
+
+const EditButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  margin: 0 10px;
+  background: #0ac620;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+
+const IsCompletedButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  background: #f2a115;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+const DeleteButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  background: #f54242;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+
 function EditTodo() {
   return (
     <div>
